364ca06e0d994fdfaf1077bcc354c35279f9a775
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-08-14 05:40:59 +0000

    new cookie class

--- themes/roselife/js/main.js
+++ themes/roselife/js/main.js
@@ -107,20 +107,105 @@ $(document).ready(function() {
    // for logged-in user
    if (uid > 0)
    {
-      var BBEditor = $('.bbcode_editor textarea');
-      var titleEditor = $('#edit-title');
-      var editorDiv = $('#editor-div');
-      var editorForm = $('#editor-form');
-      var fileTable = $('#ajax-file-list');
-      var fileTableBody = $('tbody', fileTable);
-      var TextEditor = $('#TextEditor');
-
       var pmCount = $.cookie('pmCount');
       if (pmCount > 0)
       {
          $("a#pm").append('<span style="color:red;"> (' + pmCount + ') <span>');
       }
 
-      BBEditor.markItUp(myBBCodeSettings);
+      editorBody.markItUp(myBBCodeSettings);
+
+      var editorForm = $('#bbcode_editor');
+      if (editorForm.length)
+      {
+         var editorBody = $('#bbcode_editor textarea'),
+             editorTitle = $('#bbcode_editor .node_title'),
+             fileTable = $('#ajax_file_list'),
+             fileTableBody = $('tbody', fileTable);
+
+         // button actions
+         $('button.delete').click(function(e) {
+            var answer = confirm("此操作不可恢复，您确认要删除该内容吗？");
+            if (answer)
+            {
+               window.location = $(this).attr('data-action');
+            }
+         });
+
+         $('button.reply').click(function(e) {
+            editorForm.attr('action', $(this).attr('data-action'));
+            editorTitle.hide();
+            fileTable.hide();
+            fileTableBody.children().remove();
+
+            editorBody.val('').focus();
+
+            window.scrollTo(0, editorForm.offset().top);
+         });
+
+         $('button.quote').click(function(e) {
+            editorForm.attr('action', $(this).attr('data-action'));
+            editorTitle.hide();
+            fileTable.hide();
+            fileTableBody.children().remove();
+            var data = $($(this).attr('data-raw'));
+            var author = data.find('pre.username').html();
+            var quoteText = '[quote="' + author + '"]' + data.find('pre.body').html() + '[/quote]\n';
+            editorBody.val('').focus();
+            $.markItUp({
+               replaceWith: quoteText
+            });
+
+            window.scrollTo(0, editorForm.offset().top);
+         });
+
+         $('button.edit').click(function(e) {
+            var action = $(this).attr('data-action');
+            editorForm.attr('action', action);
+            if (action.split('/')[1] == 'node')
+            {
+               editorTitle.show();
+            }
+            else
+            {
+               editorTitle.hide();
+            }
+
+            fileTableBody.children().remove();
+            var files = $.parseJSON(data.find('span.files').html()); // may return null
+
+            if (files instanceof Array && files.length > 0)
+            {
+               for (var i = 0; i < files.length; i++) {
+                  var fid = files[i].fid;
+                  var path = files[i].path;
+                  var imageExt = new Array('jpeg', 'gif', 'png');
+                  var fileExt = path.split('.').pop();
+                  var bbcode;
+
+                  if (imageExt.indexOf(fileExt) >= 0) {
+                     bbcode = '[img]' + path + '[/img]';
+                  }
+                  else {
+                     bbcode = '[file="' + path + '"]' + files[i].name + '[/file]';
+                  }
+
+                  var row = '<tr id="editfile-' + fid + '">' +
+                      '<td><input type="text" size="30" maxlength="30" name="files[' + fid + '][name]" id="editfile-' + fid + '-name" value="' + files[i].name + '"><input type="text" style="display:none;" name="files[' + fid + '][path]" value="' + path + '"></td>' +
+                      '<td style="padding: 0 10px;">' + bbcode + '</td><td style="text-align: center;"><button class="ajax_file_delete">删除</button></td></tr>';
+                  fileTableBody.append(row);
+               }
+               fileTable.show();
+            }
+            else
+            {
+               fileTable.hide();
+            }
+
+            editorBody.val($($(this).attr('data-raw')).find('pre.body').html()).focus();
+
+            window.scrollTo(0, editorForm.offset().top);
+         });
+      }
    }
 });
