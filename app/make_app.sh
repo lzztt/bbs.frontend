cdb4ae802154e116426f526f0d3ff81e670f2da4
Long <15257546+lzztt@users.noreply.github.com>
2015-01-19 05:20:03 +0000

    update app

--- app/make_app.sh
+++ app/make_app.sh
@@ -5,9 +5,9 @@ appnew="$1.$time"
 if [ -d "$appdir" ]; then
     cp -pr $appdir $appnew
     sed -i "s/__HEAD__/$time/g" $appnew/index.html $appnew/app.js
-    sed -i 's!/app.js"!/min.js"!g' $appnew/index.html
-    java -jar /home/web/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar -v --type js --charset utf-8 -o $appnew/min.js $appnew/app.js > $appnew/min.log 2>&1
-    gzip -c $appnew/min.js > $appnew/min.js.gz
+    sed -i 's!/app.js"!/'$time'.min.js"!g' $appnew/index.html
+    java -jar /home/web/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar -v --type js --charset utf-8 -o $appnew/$time.min.js $appnew/app.js > $appnew/min.log 2>&1
+    gzip -c $appnew/$time.min.js > $appnew/$time.min.js.gz
     for i in $appnew/*.tpl.html; do gzip -c $i > $i.gz; done
     echo -n "$appnew" > "$1.current"
     echo "new app created: $appnew"
