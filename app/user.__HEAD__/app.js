e6dce654c7fd9e6e9f1153a6ef325bbd7cdd3d22
Mika He <mika.he@geekpush.com>
2015-02-10 20:51:08 +0000

    add user delete functionality

--- app/user.__HEAD__/app.js
+++ app/user.__HEAD__/app.js
@@ -402,6 +402,20 @@ userApp.controller('ProfileCtrl', ['$scope', '$routeParams', '$cookies', '$http'
       $http.get('/api/user/' + uid).success(function (data) {
          if (validateResponse(data)) {
             $scope.user = data;
+            
+            if (cache.get('uid') == 1) {
+               $scope.isAdmin = true;
+               $scope.deleteUser = function () {
+                  var answer = confirm('此操作不可恢复，确认删除此用户: ' + $scope.user.username + ' (' + uid + ')?');
+                  if (answer) {
+                     $http.get('/api/user/' + uid + '?action=delete').success(function (data) {
+                        if (validateResponse(data)) {
+                           alert( '用户 ' + $scope.user.username + ' ID: '+ uid + ' 已经从系统中删除。');
+                        }
+                     });
+                  }
+               }
+            }
 
             var avatar = data.avatar ? data.avatar : '/data/avatars/avatar0' + Math.ceil(Math.random() * 5) + '.jpg';
             if (uid == cache.get('uid')) {
