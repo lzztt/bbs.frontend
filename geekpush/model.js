7d75f7bede4684fe248ad2cb50071ae38a1c9fa2
Alex Tian <alex.tian@geekpush.com>
2015-10-17 18:42:02 -0500

    working geekpush

---
+++ geekpush/model.js
@@ -0,0 +1,53 @@
+var c = {
+  title: '极客推动工作室',
+  slogan: '简单专注 且有爱心',
+  sections: [
+    {
+      type: "text",
+      title: "我们是谁",
+      body: [
+        "极客（Geek），是一群拒绝普通，专注于自己的兴趣爱好，并把事情做到完美极致的人。",
+        "而科技界的极客们——我们，则是相信可以通过自己的技术和热情，让沟通变简单，生活变丰富。"
+      ]
+    },
+    {
+      type: "text-image",
+      title: "我们做什么",
+      body: [
+        {
+          title: "宗旨与信念",
+          body: "本着简单专注，且有爱心的信念，我们致力于用自己的技能搭建有效沟通的网络平台。我们相信一个卓越的本地论坛会极大的丰富和便捷海外华人的生活。",
+          image: "/geekpush/image/do1.png"
+        },
+        {
+          title: "两个论坛 两个平台",
+          body: "六年前，我们创建了缤纷休斯顿，一年前缤纷达拉斯上线。没有垃圾信息，没有打折推广，给用户一个纯粹的信息交流环境。每年义务组织“四月看野花”和“七夕单身聚会”活动，服务用户，加深羁绊。",
+          image: "/geekpush/image/do2.jpg"
+        },
+        {
+          title: "愿景与未来",
+          body: "我们在不断开发论坛新版本。为了用户使用方便，从电脑版到平板手机自适应版，再到马上即将上线的app版。我们也在不断丰富论坛内容，推出免费电子黄页活动，建立本地生活信息库。",
+          image: "/geekpush/image/do3.jpg"
+        }
+      ]
+    },
+    {
+      type: "card",
+      title: "联系我们",
+      body: [
+        {
+          title: "缤纷休斯顿",
+          body: ["Web: http://www.houstonbbs.com", "Email: admin@houstonbbs.com"]
+        },
+        {
+          title: "缤纷达拉斯",
+          body: ["Web: http://www.dallasbbs.com", "Email: admin@dallasbbs.com"]
+        },
+        {
+          title: "微信公众号",
+          body: ["/geekpush/image/wechat.jpg"]
+        }
+      ]
+    },
+  ],
+};
