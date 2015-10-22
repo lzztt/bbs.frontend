18191e206f393375b7f20629e05da962b8c509e4
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2015-10-21 22:59:06 -0500

    working password page

--- app/web.__HEAD__/guest/Password.js
+++ app/web.__HEAD__/guest/Password.js
@@ -2,6 +2,7 @@
 
 var Password = {
   controller: function() {
+    console.log('# Password.controller');
     this.email = m.prop('');
     this.username = m.prop('');
 
@@ -16,8 +17,28 @@ var Password = {
     this.return = m.prop();
     this.error = m.prop();
 
+    var onIdentRequestSuccess = function(data) {
+      console.log('ident request finished');
+      if (validateResponse(data)) {
+        alert("安全验证码已经成功发送到您的注册邮箱 " + this.email + " ，请检查email。\n如果您的收件箱内没有此电子邮件，请检查电子邮件的垃圾箱，或者与网站管理员联系。");
+      }
+      else {
+        this.step = 0;
+      }
+    }.bind(this);
+
+    var onPasswordRequestSuccess = function(data) {
+      console.log('password request finished');
+      if (validateResponse(data)) {
+        alert("您的新密码已经设置成功");
+      }
+      else {
+        this.step = 1;
+      }
+    }.bind(this);
+
     this.submit = function(ev) {
-      console.log(this.step);
+      console.log('submit ' + this.step);
       ev.preventDefault();
       ev.stopPropagation();
       ev.stopImmediatePropagation();
@@ -35,7 +56,20 @@ var Password = {
           }
         }
 
-        m.request({method: "POST", url: "/api/identificationcode", data: {email: this.email(), username: this.username(), captcha: this.captcha()}});
+        m.request({
+          method: "POST",
+          url: "/api/identificationcode",
+          data: {email: this.email(), username: this.username(), captcha: this.captcha()},
+          serialize: function(data) {
+            return m.route.buildQueryString(data)
+          },
+          config: function(xhr) {
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+          }}).then(onIdentRequestSuccess);
+
+        // clear captcha, just in case need to re-input
+        this.captcha('');
         $('div.captcha', ev.target).remove();
       }
       else
@@ -55,15 +89,26 @@ var Password = {
           return false;
         }
 
-        m.request({method: "POST", url: "/api/user/" + this.security(), data: {password: this.password()}});
+        m.request({
+          method: "POST",
+          url: "/api/user/" + this.security() + '?action=put',
+          data: {password: this.password()},
+          serialize: function(data) {
+            return m.route.buildQueryString(data)
+          },
+          config: function(xhr) {
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+          }}).then(onPasswordRequestSuccess);
       }
 
       // move to next step
       this.step++;
+      console.log('submit end');
     }.bind(this);
   },
   view: function(ctrl) {
-    console.log('### Password view');
+    console.log('# Password.view');
     if (ctrl.step == 0) {
       var fields = [
         m.component(Form.Input, {type: 'email', label: '注册邮箱', name: 'email', value: ctrl.email}),
