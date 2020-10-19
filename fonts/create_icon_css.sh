3639a44bd6bb5e956eab3ed4b903bef79b586da1
Long <15257546+lzztt@users.noreply.github.com>
2020-10-18 22:58:39 -0700

    remove old files

--- fonts/create_icon_css.sh
+++
@@ -1,3 +0,0 @@
-# cd fontello-a6f36dd3 
-grep 'class="i-code"' demo.html | sed -e 's/.*i-name">/./' -e 's/<[^>]*>//g' -e 's/0x/:before { content:"\\/' -e 's/$/"; }/'
-# > icon.css
--- themes/default/ff_min.sh
+++
@@ -1,4 +0,0 @@
- time=`date +%s`
- cat js/jquery-1.8.3.js js/FFcoin-slider.js  js/FFmain.js > js/fyfm-all-$time.js
- java -jar /home/web/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar -v --type js --charset utf-8 -o js/fyfm-min-$time.js js/fyfm-all-$time.js
- gzip -c js/fyfm-min-$time.js > js/fyfm-min-$time.js.gz
