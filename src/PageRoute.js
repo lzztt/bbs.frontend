e783aebb6abe42134d0adda21778d523dc880fd2
Long <15257546+lzztt@users.noreply.github.com>
2022-03-23 09:23:04 -0700

    refactor PrivateRoute

--- src/PageRoute.js
+++ src/PageRoute.js
@@ -20,28 +20,6 @@ import AdAdd from "./ad/Add";
 import AdPayment from "./ad/Payment";
 import NotFound from "./NotFound";
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, isAuthenticated, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        isAuthenticated ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/user/login",
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
 function PageRoute({ loggedIn, setLoggedIn }) {
   const location = useLocation();
   const history = useHistory();
@@ -65,58 +43,66 @@ function PageRoute({ loggedIn, setLoggedIn }) {
 
   const userId = cache.get("uid") || 0;
 
-  return (
+  return loggedIn ? (
     <Switch>
       <Route path="/user" exact>
-        {!loggedIn ? <Redirect to="/user/login" /> : <User />}
+        <User />
       </Route>
       <Route path="/user/login">
-        {loggedIn ? <NotFound /> : <Login setLoggedIn={setLoggedIn} />}
+        <NotFound />
       </Route>
       <Route path="/user/passwordlogin">
-        {loggedIn ? <NotFound /> : <PasswordLogin setLoggedIn={setLoggedIn} />}
+        <NotFound />
       </Route>
       <Route path="/user/mailbox" exact>
         <Redirect to="/user/mailbox/inbox" />
       </Route>
-      <PrivateRoute path="/user/mailbox/:mailbox" isAuthenticated={loggedIn}>
+      <Route path="/user/mailbox/:mailbox">
         <Mailbox />
-      </PrivateRoute>
-      <PrivateRoute path="/user/pm/:messageId" isAuthenticated={loggedIn}>
-        <Message />
-      </PrivateRoute>
-      <PrivateRoute path="/user/bookmark" isAuthenticated={loggedIn}>
-        <Bookmark />
-      </PrivateRoute>
-      <Route path="/user/logout">
-        {!loggedIn ? (
-          <Redirect to="/user/login" />
-        ) : (
-          <Logout setLoggedIn={setLoggedIn} />
-        )}
       </Route>
-      <PrivateRoute path="/user/:userId" isAuthenticated={loggedIn}>
+      <Route path="/user/pm/:messageId">
+        <Message />
+      </Route>
+      <Route path="/user/bookmark">
+        <Bookmark />
+      </Route>
+      <Route path="/user/logout">
+        <Logout setLoggedIn={setLoggedIn} />
+      </Route>
+      <Route path="/user/:userId">
         <User />
-      </PrivateRoute>
-      <PrivateRoute
-        path="/adadmin/add"
-        isAuthenticated={loggedIn && userId === 1}
-      >
-        <AdAdd />
-      </PrivateRoute>
-      <PrivateRoute
-        path="/adadmin/payment"
-        isAuthenticated={loggedIn && userId === 1}
-      >
-        <AdPayment />
-      </PrivateRoute>
-      <PrivateRoute
-        path="/adadmin"
-        exact
-        isAuthenticated={loggedIn && userId === 1}
-      >
-        <AdHome />
-      </PrivateRoute>
+      </Route>
+      <Route path="/adadmin/add">
+        {userId === 1 ? <AdAdd /> : <NotFound />}
+      </Route>
+      <Route path="/adadmin/payment">
+        {userId === 1 ? <AdPayment /> : <NotFound />}
+      </Route>
+      <Route path="/adadmin" exact>
+        {userId === 1 ? <AdHome /> : <NotFound />}
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  ) : (
+    <Switch>
+      <Route path="/user/login">
+        <Login setLoggedIn={setLoggedIn} />
+      </Route>
+      <Route path="/user/passwordlogin">
+        <PasswordLogin setLoggedIn={setLoggedIn} />
+      </Route>
+      <Route path="/user/logout">
+        <Logout setLoggedIn={setLoggedIn} />
+      </Route>
+      <Route path={["/user", "/adadmin"]}>
+        <Redirect to={{
+          pathname: "/user/login",
+          state: { from: location },
+        }}
+        />
+      </Route>
       <Route path="*">
         <NotFound />
       </Route>
